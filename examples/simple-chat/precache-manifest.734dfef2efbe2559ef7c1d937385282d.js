aba9f84b92d5fe99295cab89e00040893d5746aa
afrokick <devbyru@gmail.com>
2019-09-11 15:36:47 +0300

    add simple-chat example

---
+++ examples/simple-chat/precache-manifest.734dfef2efbe2559ef7c1d937385282d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66921209c37679b7723ecc4e4f827e5b",
+    "url": "/examples/simple-chat/index.html"
+  },
+  {
+    "revision": "9671e58ce3a918c7620f",
+    "url": "/examples/simple-chat/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "60521e8b6d38e1e0217a",
+    "url": "/examples/simple-chat/static/js/2.85a6cd9e.chunk.js"
+  },
+  {
+    "revision": "9671e58ce3a918c7620f",
+    "url": "/examples/simple-chat/static/js/main.0d8c287d.chunk.js"
+  },
+  {
+    "revision": "2477df162ce9749bd2cb",
+    "url": "/examples/simple-chat/static/js/runtime~main.c2befd03.js"
+  }
+]);
