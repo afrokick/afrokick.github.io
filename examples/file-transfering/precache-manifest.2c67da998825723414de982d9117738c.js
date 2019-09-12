404375104669efba3d830666c79f09cdabf2a9bb
afrokick <devbyru@gmail.com>
2019-09-12 14:49:11 +0300

    add new file-transfering example

---
+++ examples/file-transfering/precache-manifest.2c67da998825723414de982d9117738c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f65296618858ae5007fc89082dcea8",
+    "url": "/examples/file-transfering/index.html"
+  },
+  {
+    "revision": "60a61d3ef7a6bd60c0b5",
+    "url": "/examples/file-transfering/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "fae4d38c7eecb90cabde",
+    "url": "/examples/file-transfering/static/js/2.1d475065.chunk.js"
+  },
+  {
+    "revision": "60a61d3ef7a6bd60c0b5",
+    "url": "/examples/file-transfering/static/js/main.f7f18945.chunk.js"
+  },
+  {
+    "revision": "6741737516c49b243aa2",
+    "url": "/examples/file-transfering/static/js/runtime~main.02e3cbcc.js"
+  }
+]);
